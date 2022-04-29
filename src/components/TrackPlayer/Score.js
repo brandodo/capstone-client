e4b3d475629c7aab4cb4ca1a2a6f5327c0f638c1
brandodo <brandonong93@gmail.com>
2022-04-29 00:44:37 -0400

    config heroku

--- src/components/TrackPlayer/Score.js
+++ src/components/TrackPlayer/Score.js
@@ -3,9 +3,11 @@ import React from "react";
 export default function Score({ points, combo, multiplier }) {
   return (
     <>
-      <h2 className="sidebar__playerCombo">{`Combo: ${combo}`}</h2>
-      <h2 className="sidebar__playerMultiplier">{`Multiplier: x${multiplier}`}</h2>
       <h2 className="sidebar__playerScore">{`Score: ${points}`}</h2>
+      <div className="sidebar__comboContainer">
+        <h2 className="sidebar__playerCombo">{`Combo: ${combo}`}</h2>
+        <h2 className="sidebar__playerMultiplier">{`Multiplier: x${multiplier}`}</h2>
+      </div>
     </>
   );
 }
