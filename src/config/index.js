9cc9f93f8c3930ba1e1c773aff4b4559a97e6026
brandodo <brandonong93@gmail.com>
2022-04-28 21:54:29 -0400

    config for heroku

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 // TODO update the urls to match your deployed heroku app and local development port
 
 export const API_URL =
-  process.env.NODE_ENV === 'production'
-    ? 'https://express-bookshelf.herokuapp.com'
-    : 'http://localhost:8000';
+  process.env.NODE_ENV === "production"
+    ? "https://aim-beats-server.herokuapp.com/"
+    : "http://localhost:8000";
